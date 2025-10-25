2a31c3edbd12b3087d2b2034236cc4f52e8f40c3
Haleem-code <abdulhaleemayomide@gmail.com>
2025-10-25 02:57:39 +0100

    Added the rustbook guessing game folder

---
+++ rng-game/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main(){
+    println!("Guess the number!");
+    loop{
+    print!("Please input your guess: ");
+    let mut guess = String::new();
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!("You guessed: {guess}");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=5);
+    println!("The secret number is: {secret_number}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
+}
